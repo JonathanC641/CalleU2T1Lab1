574f28ec940dcfd8c2f22f3d9549eef1db950e05
1E10 Laptop <sample@bths.edu>
2022-09-23 10:38:10 -0400

    Code

---
+++ src/RectangleRunner.java
@@ -0,0 +1,7 @@
+public class RectangleRunner {
+    public static void main(String[] args) {
+        Rectangle rect3 = new Rectangle(5, 6);
+        rect3.printArea();
+        rect3.printPerimeter();
+    }
+}
